077c0fbe6bd52e5b2a2f6fc4b201f490e367f4b8
manong553 <96342571+manong553@users.noreply.github.com>
2021-12-18 22:38:18 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||李|诗|佳|祝|你|生|日|快|乐|❤|');
+      S.UI.simulate('|#countdown 3||李|诗|佳|祝你|生日|快乐|❤|');
     }
 
     S.Drawing.loop(function () {
